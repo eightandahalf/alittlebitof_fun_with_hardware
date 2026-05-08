0b47efb6935184424440a2c4eabe341f6f3a5cd1
Hleb Arbuzau <hleb.arbuzau@gmail.com>
2026-05-08 19:46:46 +0300

    +

---
+++ websocket_client/target/build/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,27 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
+  file(REMOVE_RECURSE
+  "bootloader\\bootloader.bin"
+  "bootloader\\bootloader.elf"
+  "bootloader\\bootloader.map"
+  "ca_cert.pem.S"
+  "client_cert.pem.S"
+  "client_key.pem.S"
+  "config\\sdkconfig.cmake"
+  "config\\sdkconfig.h"
+  "esp-idf\\mbedtls\\x509_crt_bundle"
+  "flash_app_args"
+  "flash_bootloader_args"
+  "flash_project_args"
+  "flasher_args.json"
+  "flasher_args.json.in"
+  "ldgen_libraries"
+  "ldgen_libraries.in"
+  "project_elf_src_esp32.c"
+  "websocket_example.bin"
+  "websocket_example.map"
+  "x509_crt_bundle.S"
+  )
+endif()
